5e7bd141ae4b16536de0abf5bbe1df7943495d1e
Eric Chan <chenwenjie@ppmoney.com>
2015-09-23 11:48:38 +0800

    HI@!

--- GitTest/GitTest/Controllers/HomeController.cs
+++ GitTest/GitTest/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace GitTest.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "hi!";
 
             return View();
         }
